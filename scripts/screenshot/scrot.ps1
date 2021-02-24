144c9ad04b8f336149c029aa91dbbdd9c5a12998
mushfiq814 <mushfiq@mail.usf.edu>
2021-02-24 11:36:34 -0500

    new(psscripts): add screenshot script

---
+++ scripts/screenshot/scrot.ps1
@@ -0,0 +1,15 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+}
+
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 2560, 1440)
+screenshot $bounds "C:\Users\mushf\Downloads\screenshot.png"
