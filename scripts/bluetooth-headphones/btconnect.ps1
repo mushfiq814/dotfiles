f3f157694f7e66076ab1cb0538d476130383ecd3
Mushfiq Mahmud <mushfiq8194@gmail.com>
2021-05-03 16:32:57 -0400

    style(scripts): better status output for btconnect script

--- scripts/bluetooth-headphones/btconnect.ps1
+++ scripts/bluetooth-headphones/btconnect.ps1
@@ -2,14 +2,16 @@ $btcom = "C:\Program Files (x86)\Bluetooth Command Line Tools\bin\btcom.exe"
 $deviceName = "WH-XB900N"
 
 # disconnect first from voice (111e) and music (110b) service
-$d1 = '& "$btcom" -n $deviceName -r -s110b'
-$d2 = '& "$btcom" -n $deviceName -r -s111e'
-Invoke-Expression $d1
-Invoke-Expression $d2
+Write-Host "disconnecting $deviceName from music service"
+Invoke-Expression '& "$btcom" -n $deviceName -r -s110b'
+Write-Host "disconnecting $deviceName from voice service"
+Invoke-Expression '& "$btcom" -n $deviceName -r -s111e'
+
 # wait
 Start-Sleep -Seconds 2
-# # connect to voice (111e) and music (110b) service
-$c1 = '& "$btcom" -n $deviceName -c -s110b'
-$c2 = '& "$btcom" -n $deviceName -c -s111e'
-Invoke-Expression $c1
-Invoke-Expression $c2
+
+# connect to voice (111e) and music (110b) service
+Write-Host "reconnecting $deviceName to music service"
+Invoke-Expression '& "$btcom" -n $deviceName -c -s110b'
+Write-Host "reconnecting $deviceName to voice service"
+Invoke-Expression '& "$btcom" -n $deviceName -c -s111e'
