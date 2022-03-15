edc388f7bdda36084333dc97b32166ad8140551e
mushfiq814 <mmahmud@freedomdebtrelief.com>
2022-03-15 12:58:57 -0400

    chore(neovim): add cmp, gitsigns, +1 plugins

--- neovim/.config/nvim/lua/plugin-settings/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugin-settings/nvim-cmp.lua
@@ -1,53 +1,144 @@
 vim.o.completeopt = "menu,menuone,noselect"
 
--- Setup nvim-cmp.
-local cmp = require'cmp'
+-- Setup nvim-cmp and luasnip
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then return end
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then return end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+--   פּ ﯟ   some other good icons
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
+local check_backspace = function()
+  local col = vim.fn.col "." - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
 
 cmp.setup({
   snippet = {
-    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
+    },
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif check_backspace() then
+        fallback()
+      else
+        fallback()
+      end
+    end, {
+        "i",
+        "s",
+      }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
-  sources = cmp.config.sources({
-  { name = 'nvim_lsp' },
-  { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-    })
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  sources = {
+  { name = "nvim_lsp" },
+  { name = "luasnip" },
+  { name = "buffer" },
+  { name = "path" },
+  },
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+  documentation = {
+    -- border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    border = { " ", " ", " ", " ", " ", " ", " ", " " },
+  },
+  experimental = {
+    ghost_text = false,
+    native_menu = false,
+  },
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
   sources = cmp.config.sources({
-  { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you installed it.
   }, {
     { name = 'buffer' },
-    })
+  })
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
   sources = {
-  { name = 'buffer' }
+    { name = 'buffer' }
   }
 })
 
@@ -57,12 +148,11 @@ cmp.setup.cmdline(':', {
   { name = 'path' }
   }, {
     { name = 'cmdline' }
-    })
+  })
 })
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['jsonls'].setup {
   capabilities = capabilities
 }
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -17,8 +17,8 @@ end
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd [[
   augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]]
 
@@ -38,68 +38,74 @@ packer.init {
 }
 
 require('packer').startup(function ()
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-	-- Programming Language Plugins
-	use 'godlygeek/tabular'
-	use 'mattn/emmet-vim'
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } ; require('plugin-settings/treesitter')
-	-- use 'lewis6991/spellsitter.nvim' ; require('plugin-settings/spellsitter')
-	use 'neovim/nvim-lspconfig' ; require('plugin-settings/nvim-lsp')
-	use 'williamboman/nvim-lsp-installer' ; require('plugin-settings/lsp-installer')
+  -- Programming Language Plugins
+  use 'godlygeek/tabular'
+  use 'mattn/emmet-vim'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } ; require('plugin-settings/treesitter')
+  -- use 'lewis6991/spellsitter.nvim' ; require('plugin-settings/spellsitter')
+  use 'RRethy/nvim-treesitter-textsubjects' ; require('plugin-settings.treesitter-text-subjects')
+  use 'neovim/nvim-lspconfig' ; require('plugin-settings/nvim-lsp')
+  use 'williamboman/nvim-lsp-installer' ; require('plugin-settings/lsp-installer')
 
-	-- use 'hrsh7th/cmp-nvim-lsp'
-	-- use 'hrsh7th/cmp-buffer'
-	-- use 'hrsh7th/cmp-path'
-	-- use 'hrsh7th/cmp-cmdline'
-	-- use 'hrsh7th/nvim-cmp' ; require('plugin-settings/nvim-cmp')
-	-- use 'hrsh7th/cmp-vsnip'
-	-- use 'hrsh7th/vim-vsnip'
-	use 'hrsh7th/nvim-compe' ; require('plugin-settings/nvim-compe')
+  -- cmp plugins
+  use 'hrsh7th/nvim-cmp' ; require('plugin-settings/nvim-cmp') -- completion plugin
+  use 'hrsh7th/cmp-buffer' -- buffer completions
+  use 'hrsh7th/cmp-path' -- path completions
+  use 'hrsh7th/cmp-cmdline' -- cmdline completions
+  use 'saadparwaiz1/cmp_luasnip' -- snippet completions
+  use 'hrsh7th/cmp-nvim-lsp'
 
-	-- Navigation Plugins
-	use 'nvim-telescope/telescope.nvim' ; require('plugin-settings/telescope')
-	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-	use 'masukomi/vim-markdown-folding'
-	use 'nvim-lua/plenary.nvim'
-	use 'psliwka/vim-smoothie'
-	use 'rstacruz/vim-closer'
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
-	-- UI Colors
-	use 'kyazdani42/nvim-web-devicons'
-	use 'kyazdani42/nvim-tree.lua' ; require('plugin-settings/nvim-tree')
-	use 'hoob3rt/lualine.nvim' ; require('plugin-settings/lualine')
-	use 'nanozuki/tabby.nvim' ; require('plugin-settings/tabby')
+  -- Navigation Plugins
+  use 'nvim-telescope/telescope.nvim' ; require('plugin-settings/telescope')
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use 'masukomi/vim-markdown-folding'
+  use 'nvim-lua/plenary.nvim'
+  use 'psliwka/vim-smoothie'
+  use 'rstacruz/vim-closer'
 
-	-- Colorschemes
-	use 'morhetz/gruvbox'
-	use 'Shadorain/shadotheme'
-	use 'whatyouhide/vim-gotham'
-	use 'sainnhe/sonokai'
-	use 'NLKNguyen/papercolor-theme'
-	use 'navarasu/onedark.nvim' ; require('plugin-settings/onedark')
+  -- UI Colors
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua' ; require('plugin-settings/nvim-tree')
+  use 'hoob3rt/lualine.nvim' ; require('plugin-settings/lualine')
+  use 'nanozuki/tabby.nvim' ; require('plugin-settings/tabby')
 
-	use 'lukas-reineke/indent-blankline.nvim' ; require('plugin-settings/indent-blankline')
-	use 'junegunn/goyo.vim'
-	use 'goolord/alpha-nvim' ; require('plugin-settings/alpha')
-	use 'norcalli/nvim-colorizer.lua' ; require'colorizer'.setup()
-	use 'fladson/vim-kitty'
+  -- Colorschemes
+  use 'morhetz/gruvbox'
+  use 'Shadorain/shadotheme'
+  use 'whatyouhide/vim-gotham'
+  use 'sainnhe/sonokai'
+  use 'NLKNguyen/papercolor-theme'
+  use 'navarasu/onedark.nvim' ; require('plugin-settings/onedark')
 
-	use 'folke/twilight.nvim' ; require('plugin-settings/twilight')
+  use 'lukas-reineke/indent-blankline.nvim' ; require('plugin-settings/indent-blankline')
+  use 'junegunn/goyo.vim'
+  use 'goolord/alpha-nvim' ; require('plugin-settings/alpha')
+  use 'norcalli/nvim-colorizer.lua' ; require'colorizer'.setup()
+  use 'fladson/vim-kitty'
 
-	-- Development Tools
-	use 'tpope/vim-fugitive'
-	use 'mbbill/undotree'
-	use 'airblade/vim-gitgutter' ; require('plugin-settings/gitgutter')
-	use 'tpope/vim-surround'
-	use 'tpope/vim-commentary'
-	use 'jkramer/vim-checkbox'
+  use 'folke/twilight.nvim' ; require('plugin-settings/twilight')
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+  -- Development Tools
+  use 'tpope/vim-fugitive'
+  use 'mbbill/undotree'
+  -- use 'airblade/vim-gitgutter' ; require('plugin-settings/gitgutter')
+  use 'tpope/vim-surround'
+  use 'tpope/vim-commentary'
+  use 'jkramer/vim-checkbox'
+  use 'tyru/capture.vim'
+  use 'lewis6991/gitsigns.nvim' ; require('plugin-settings.gitsigns')
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
 
