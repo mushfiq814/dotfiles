539b8c7b713dd04d195ec37324324f09ca87eec9
Mushfiq Mahmud <mushfiq8194@gmail.com>
2022-08-28 18:59:13 -0400

    chore(neovim/treesitter): add settings for octo

--- neovim/.config/nvim/lua/plugin-settings/treesitter.lua
+++ neovim/.config/nvim/lua/plugin-settings/treesitter.lua
@@ -8,3 +8,6 @@ nvim_treesitter.setup {
 
 vim.o.foldmethod = 'expr'
 vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+
+local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
+ft_to_parser.markdown = "octo" -- the someft filetype will use the python parser and queries.
