9dc1d23dc87f84618f6a493cba5f9d97dab2b731
Mushfiq Mahmud <mushfiq8194@gmail.com>
2022-08-28 18:54:47 -0400

    style(neovim/lsp-installer): wrap comment

--- neovim/.config/nvim/lua/plugin-settings/lsp-installer.lua
+++ neovim/.config/nvim/lua/plugin-settings/lsp-installer.lua
@@ -1,8 +1,9 @@
 local success, lsp_installer = pcall(require, 'nvim-lsp-installer')
 if not success then return end
 
--- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
--- or if the server is already installed).
+-- Register a handler that will be called for each installed server when it's
+-- ready (i.e. when installation is finished or if the server is already
+-- installed).
 lsp_installer.on_server_ready(function(server)
   local opts = {}
 
