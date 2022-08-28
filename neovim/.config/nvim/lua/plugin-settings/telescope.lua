da2971e5eb695fab81aa78ae0f0ba15b72b37214
Mushfiq Mahmud <mushfiq8194@gmail.com>
2022-08-28 19:00:46 -0400

    chore(neovim/telescope): reorder keybindings
    * NEW
      * <leader>br (git_branches)
      * <leader>vfg (open live_grep for config dir)
    * CHANGED
      * <leader>s -> <leader>ss (spell_suggest)
      * <leader><C-p> -> <leader>v<C-p> (open file preview for config dir)
    * REMOVED
      * <leader>ff (git_files)

--- neovim/.config/nvim/lua/plugin-settings/telescope.lua
+++ neovim/.config/nvim/lua/plugin-settings/telescope.lua
@@ -24,21 +24,28 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 keymap('n', '<C-p>', ':Telescope find_files find_command=rg,--hidden,--files prompt_prefix=\\ 🔍\\ <CR>', opts)
-keymap('n', '<leader>ff', ':Telescope git_files prompt_prefix=\\ 🔍\\ <CR>', opts)
+keymap('n', '<leader>b',  ':Telescope buffers prompt_prefix=\\ 🔍\\ <CR>', opts)
+keymap('n', '<leader>br', ':Telescope git_branches prompt_prefix=\\ \\ <CR>', opts)
 keymap('n', '<leader>fg', ':Telescope live_grep prompt_prefix=\\ 🔍\\ <CR>', opts)
-keymap('n', '<leader>b', ':Telescope buffers prompt_prefix=\\ 🔍\\ <CR>', opts)
-keymap('n', '<leader>h', ':Telescope help_tags prompt_prefix=\\ 🔍\\ <CR>', opts)
-keymap('n', '<leader>s', ':Telescope spell_suggest prompt_prefix=\\ ✅\\ <CR>', opts)
+keymap('n', '<leader>gr', ':lua require(\'telescope.builtin\').lsp_references(require(\'telescope.themes\').get_ivy({}))<CR>', opts)
+keymap('n', '<leader>h',  ':Telescope help_tags prompt_prefix=\\ 🆘\\ <CR>', opts)
 keymap('n', '<leader>rc', ':Telescope oldfiles prompt_prefix=\\ ⏰\\ <CR>', opts)
-keymap('n', '<leader>gr', ':lua require(\'telescope.builtin\').lsp_references(require(\'telescope.themes\').get_ivy({}))<CR><ESC>', opts)
-keymap('n', '<leader>rr', ':Telescope resume prompt_prefix=\\ ♦️\\ <CR>', opts)
+keymap('n', '<leader>rr', ':Telescope resume prompt_prefix=\\ ⏰\\ <CR>', opts)
+keymap('n', '<leader>ss', ':Telescope spell_suggest prompt_prefix=\\ ✅\\ <CR>', opts)
 
 -- custom pickers
 
 -- open file picker for neovim settings
 keymap(
   'n',
-  '<leader><C-p>',
+  '<leader>v<C-p>',
   ':lua require\'telescope.builtin\'.find_files({ cwd = \'~/.config/nvim\' })<cr>',
+  opts
+)
+-- open live grep for neovim settings
+keymap(
+  'n',
+  '<leader>vfg',
+  ':lua require\'telescope.builtin\'.live_grep({ cwd = \'~/.config/nvim\' })<cr>',
   opts
 )
