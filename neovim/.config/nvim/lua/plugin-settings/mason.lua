e1e69d68153badde992b36be39ad1aff935e300e
Mushfiq Mahmud <mushfiq8194@gmail.com>
2022-10-07 01:51:27 -0400

    chore(neovim): add server list for mason

--- neovim/.config/nvim/lua/plugin-settings/mason.lua
+++ neovim/.config/nvim/lua/plugin-settings/mason.lua
@@ -3,9 +3,11 @@ if not status_ok_1 then return end
 local status_ok_2, mason = pcall(require, "mason")
 if not status_ok_2 then return end
 
+mason.setup()
 mason_lsp_config.setup({
-  ensure_installed = servers,
+  ensure_installed = {
+    "tsserver",
+    "jsonls",
+  },
   automatic_installation = true
 })
-
-mason.setup()
