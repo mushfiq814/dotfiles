de5ab7aa387885337860d6451f300042a4247b19
Mushfiq Mahmud <mushfiq8194@gmail.com>
2022-10-07 01:47:02 -0400

    chore(neovim): update highlights

--- neovim/.config/nvim/lua/theme.lua
+++ neovim/.config/nvim/lua/theme.lua
@@ -7,31 +7,23 @@ local b = vim.bo
 local colorsFound, colors = pcall(require, 'colors')
 if not colorsFound then return end
 local themes = {
-  { name = 'gotham256'    , light = false , color = colors.NONE          },
   { name = 'gruvbox'      , light = false , color = colors.gruvbox       },
   { name = 'gruvbox-flat' , light = false , color = colors.gruvbox_flat  },
   { name = 'onedark'      , light = true  , color = colors.onedark_light },
+  { name = 'catppuccin'   , light = false , color = colors.catppuccin    },
 }
-local selectedTheme = themes[3]
+local selectedTheme = themes[1]
 COLORS = selectedTheme.color
 
 -- theme specific highlights
-if (selectedTheme.name == 'gotham256') then
-  vim.cmd('hi! LineNr guibg=NONE guifg=NONE')
-  vim.cmd('hi! SpellBad guibg=NONE guifg=NONE')
-elseif (selectedTheme.name == 'gruvbox-flat') then
-  vim.g.gruvbox_transparent = true
-  vim.g.gruvbox_dark_sidebar = false
-  vim.g.gruvbox_sidebars = {}
-  vim.g.gruvbox_dark_float = false
-  vim.g.gruvbox_hide_inactive_statusline = true
-  vim.g.gruvbox_flat_style = 'hard'
+if (selectedTheme.name == 'gruvbox-flat') then
+  -- vim.g.gruvbox_transparent = true
+  -- vim.g.gruvbox_dark_sidebar = false
+  -- vim.g.gruvbox_sidebars = {}
+  -- vim.g.gruvbox_dark_float = false
+  -- vim.g.gruvbox_hide_inactive_statusline = true
+  -- vim.g.gruvbox_flat_style = 'hard'
   vim.cmd('hi! Folded guibg=' .. COLORS.black .. ' guifg=' .. COLORS.neutral_blue)
-elseif (selectedTheme.name == 'PaperColor') then
-  vim.cmd('hi! Comment guibg=NONE guifg=#636361')
-  vim.cmd('hi! NonText guibg=NONE guifg=#636361')
-  vim.cmd('hi! LineNr guibg=NONE guifg=NONE')
-  vim.cmd('hi! SpellBad guibg=NONE guifg=NONE')
 elseif (selectedTheme.name == 'onedark') then
   vim.cmd('hi! EndOfBuffer guibg=NONE')
 elseif (selectedTheme.name == 'gruvbox') then
@@ -46,24 +38,27 @@ if (selectedTheme.light) then
   vim.cmd('set background=light')
   vim.cmd('hi! NormalFloat guibg=white')
   vim.cmd('hi! Normal guibg=white')
+  vim.cmd('hi! EndOfBuffer guibg=none')
 else
   vim.cmd('set background=dark')
   vim.cmd('hi! NormalFloat guibg=NONE')
   vim.cmd('hi! Normal guibg=NONE ctermbg=NONE')
+  vim.cmd('hi! FloatBorder guibg=NONE guifg=' .. COLORS.white)
+  vim.cmd('hi! WinSeparator guibg=NONE guifg=' .. COLORS.grey1)
+  vim.cmd('hi! NvimTreeWinSeparator guibg=NONE guifg=' .. COLORS.grey1)
 end
-vim.cmd('hi! FloatBorder guibg=NONE guifg=' .. COLORS.white)
-vim.cmd('hi! Pmenu guibg=NONE')
+-- vim.cmd('hi! Pmenu guibg=NONE')
 vim.cmd('hi! SignColumn guibg=NONE')
-vim.cmd('hi! LineNr guibg=' .. COLORS.grey0)
-vim.cmd('hi! LineNrAbove guibg=NONE guifg=' .. COLORS.grey3)
-vim.cmd('hi! LineNrBelow guibg=NONE guifg=' .. COLORS.grey3)
-vim.cmd('hi! CursorLine guibg=' .. COLORS.grey0)
-vim.cmd('hi! Folded guibg=' .. COLORS.grey2 .. ' guifg=' .. COLORS.white)
+-- vim.cmd('hi! LineNr guibg=' .. COLORS.grey0)
+-- vim.cmd('hi! LineNrAbove guibg=NONE guifg=' .. COLORS.grey3)
+-- vim.cmd('hi! LineNrBelow guibg=NONE guifg=' .. COLORS.grey3)
+-- vim.cmd('hi! CursorLine guibg=' .. COLORS.grey0)
+-- vim.cmd('hi! Folded guibg=' .. COLORS.grey2 .. ' guifg=' .. COLORS.white)
 b.syntax = 'ON'
 g.termguicolors = true
 
 -- Neovim LSP virtual diagnostics
-vim.cmd('hi! LspDiagnosticsDefaultError guifg=' .. COLORS.neutral_red)
+vim.cmd('hi! LspDiagnosticsDefaultError guifg=' .. COLORS.bright_red)
 vim.cmd('hi! LspDiagnosticsDefaultWarning guifg=' .. COLORS.neutral_yellow)
 vim.cmd('hi! LspDiagnosticsDefaultInformation guifg=' .. COLORS.neutral_blue)
 vim.cmd('hi! LspDiagnosticsDefaultHint guifg=' .. COLORS.neutral_aqua)
@@ -74,9 +69,8 @@ vim.cmd('sign define DiagnosticSignInfo  text= texthl=LspDiagnosticsDefaultIn
 vim.cmd('sign define DiagnosticSignHint  text= texthl=LspDiagnosticsDefaultHint        linehl= numhl=')
 
 -- remove both colors from regular signcolumn
-vim.cmd('hi! SignColumn guifg=# guibg=#')
+-- vim.cmd('hi! SignColumn guifg=# guibg=#')
 
 return {
 	colors = COLORS,
 }
-
