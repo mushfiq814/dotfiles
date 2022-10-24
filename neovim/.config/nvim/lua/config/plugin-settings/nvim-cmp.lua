a850320a2232788b8c6599c3cfc1e31762ddb3df
Mushfiq <mushfiq8194@gmail.com>
2022-10-24 13:14:21 -0400

    chore(neovim): update as per nvim-cmp instructions

--- neovim/.config/nvim/lua/config/plugin-settings/nvim-cmp.lua
+++ neovim/.config/nvim/lua/config/plugin-settings/nvim-cmp.lua
@@ -161,7 +161,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig_loaded, lspconfig = pcall(require, 'lspconfig')
 if not lspconfig_loaded then return end
 lspconfig['jsonls'].setup {
