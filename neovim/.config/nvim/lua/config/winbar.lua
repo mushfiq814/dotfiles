18e789f79f21eefe3f4e9219704d8983eb24ffec
Mushfiq <mushfiq8194@gmail.com>
2022-10-25 17:39:25 -0400

    chore(neovim): add winbar highlights + separate highlight for modified

--- neovim/.config/nvim/lua/config/winbar.lua
+++ neovim/.config/nvim/lua/config/winbar.lua
@@ -1,13 +1,13 @@
 local colors_loaded, colors = pcall(require, 'config/colors')
 if not colors_loaded then return end
 local lightMode = colors.mode == "light"
-local background = colors.black
-local foreground = colors.white
 
 local function filename()
   local filename = vim.fn.expand("%:t")
   if filename == '' then return '' end
-  if vim.bo.modified then filename = filename .. " ● " end
+  if vim.bo.modified then
+    filename = filename .. " %#winbarFileNameModified# ● %#Normal#"
+  end
   return "%#winbarFileName# " .. filename .. " %#Normal#"
 end
 
@@ -16,7 +16,7 @@ local function navicTags()
   if navicLoaded then
     local current_tag = nvim_navic.get_location()
     if current_tag == "" then return '' end
-    return " %#winbarTags# > " .. current_tag .. " %#Normal#"
+   return " %#winbarTags# > " .. current_tag .. " %#Normal#"
   end
 end
 
@@ -29,21 +29,19 @@ end
 
 vim.o.winbar = "%{%v:lua.MyWinbar()%}"
 
-local function highlights()
-  -- 
-  vim.cmd(
-    "highlight! winbarFileName gui=bold guifg="
-    -- .. (lightMode and background or foreground)
-    .. background
-    .. " guibg="
-    .. (lightMode and colors.bright_purple or colors.faded_purple)
-  )
-  -- 
-  vim.cmd(
-    "highlight! winbarTags guifg="
-    .. foreground
-    .. " guibg="
-    .. colors.grey1
-  )
-end
-highlights()
+local color_utils = require('config/color-utils')
+local hi = color_utils.highlight
+
+hi.winbarFileName = {
+  guifg = colors.black,
+  guibg = (lightMode and colors.bright_purple or colors.faded_purple),
+  gui = 'bold',
+}
+hi.winbarFileNameModified = {
+  guifg = colors.black,
+  guibg = colors.bright_red,
+}
+hi.winbarTags = {
+  guifg = colors.white,
+  guibg = colors.grey1,
+}
