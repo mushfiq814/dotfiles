6c6bb7fa546008e5bdf0a6a63e585105c7d2c3ce
Mushfiq <mumahmud@bills.com>
2023-05-04 12:11:03 -0400

    chore(neovim/autocommands): add markdown tab size preference

--- neovim/.config/nvim/lua/config/autocommands.lua
+++ neovim/.config/nvim/lua/config/autocommands.lua
@@ -5,6 +5,17 @@ vim.cmd('au FileType markdown nnoremap <buffer> <BS> <C-o>')
 -- align tables using Tabular
 vim.cmd('au FileType markdown nnoremap <buffer> <leader>a :Tabularize /\\|<CR>')
 
+local augroupMarkdown = vim.api.nvim_create_augroup('markdownFileEnter', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  pattern = "*.md",
+  group = augroupMarkdown,
+  callback = function()
+    vim.opt.shiftwidth = 2 -- of spaces to use for each step of (auto)indent (>>, <<)
+    vim.opt.tabstop = 2 -- of spaces that a <Tab> counts for
+    vim.opt.softtabstop = 2 -- of spaces that a <Tab> counts while editing
+  end
+})
+
 -- assign filetype to jsonc for json files to enable comments
 vim.cmd('au FileType json set filetype=jsonc')
 
