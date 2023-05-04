6d956208af963402c6f96a0d9514b19c87bf612a
Mushfiq <mumahmud@bills.com>
2023-05-04 12:12:44 -0400

    chore(neovim/null-ls): add specific filetypes

--- neovim/.config/nvim/lua/config/plugin-settings/null-ls.lua
+++ neovim/.config/nvim/lua/config/plugin-settings/null-ls.lua
@@ -5,7 +5,20 @@ null_ls.setup({
   sources = {
     null_ls.builtins.code_actions.eslint,
     null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettier.with({
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "handlebars",
+        "yaml",
+        "json",
+        "css",
+        "html",
+      },
+    }),
     -- null_ls.builtins.diagnostics.luacheck,
     -- null_ls.builtins.diagnostics.mdl,
   },
