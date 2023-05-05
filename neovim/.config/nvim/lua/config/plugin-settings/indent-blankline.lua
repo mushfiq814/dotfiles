4041c0d5d30de99f081cb9269558db1f409ad7b2
Mushfiq Mahmud <mushfiq8194@gmail.com>
2023-05-05 17:18:21 -0400

    chore(neovim): update indent-blankline config

--- neovim/.config/nvim/lua/config/plugin-settings/indent-blankline.lua
+++ neovim/.config/nvim/lua/config/plugin-settings/indent-blankline.lua
@@ -1,10 +1,32 @@
--- Special Characters: '│' '█' '▏'
-vim.g.indent_blankline_char = '▏'
-vim.g.indent_blankline_space_char = ' '
-vim.g.indent_blankline_space_char_blankline = '▏'
+local loaded, indent_blankline = pcall(require, 'indent_blankline')
+if not loaded then return end
 
--- Enable tree sitter to detect current indent level. Needs Treesitter installed
-vim.g.indent_blankline_use_treesitter = true
+local colorsFound, colors = pcall(require, 'config/colors')
+if not colorsFound then
+  vim.notify('please generate colors file using the colorscheme script')
+  return
+end
 
--- When true, automatically turns this plugin off when |nolist| is set.
-vim.g.indent_blankline_disable_with_nolist = true
+local utils = require('config/utils/color-utils')
+local hi = utils.highlight
+
+vim.opt.list = true
+vim.opt.termguicolors = true
+
+hi.IndentBlanklineChar               = { guifg = colors.grey2 , gui = 'nocombine' }
+hi.IndentBlanklineSpaceChar          = { guifg = colors.grey2 , gui = 'nocombine' }
+hi.IndentBlanklineContextChar        = { guifg = colors.white , gui = 'bold,nocombine' }
+hi.IndentBlanklineSpaceCharBlankline = { guifg = colors.white , gui = 'nocombine' }
+
+indent_blankline.setup {
+  -- Special Characters: '│' '█' '▏'
+  show_current_context = true,
+  show_current_context_start = true,
+  use_treesitter = true, -- Enable tree sitter to detect current indent level. Needs Treesitter installed
+  disable_with_nolist = true, -- When true, automatically turns this plugin off when |nolist| is set.
+  char = "▏",
+  context_char = "▏",
+  context_char_blankline = "▏",
+  space_char = " ",
+  space_char_blankline = " ",
+}
