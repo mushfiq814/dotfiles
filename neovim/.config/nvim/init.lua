76246ef86b7bc744d696eafefe4f5c42ed7a9d7e
Mushfiq Mahmud <mushfiq8194@gmail.com>
2023-05-05 16:59:10 -0400

    chore(neovim): remove other module loading from init.lua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,13 +1,1 @@
--- plugins
 require('config/plugins')
-
--- UI/UX
-require('config/theme')
-require('config/winbar')
-require('config/statusline')
-require('config/settings')
-
-require('config/keybindings')
-require('config/functions')
-require('config/commands')
-require('config/autocommands')
