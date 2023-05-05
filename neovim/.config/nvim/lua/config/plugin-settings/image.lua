0988def386cb741ead16e2247a061c210b27160b
Mushfiq Mahmud <mushfiq8194@gmail.com>
2023-05-04 22:54:00 -0400

    chore(neovim): set foreground & background for images

--- neovim/.config/nvim/lua/config/plugin-settings/image.lua
+++ neovim/.config/nvim/lua/config/plugin-settings/image.lua
@@ -1,11 +1,14 @@
 local success, image = pcall(require, 'image')
 if not success then return end
 
+-- remember to `yay -S ascii-image-converter-git`
 image.setup {
   render = {
     min_padding = 5,
     show_label = true,
     use_dither = true,
+    foreground_color = true,
+    background_color = true,
   },
   events = {
     update_on_nvim_resize = true,
