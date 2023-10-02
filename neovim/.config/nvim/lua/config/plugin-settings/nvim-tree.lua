c61d9a808de6fbaad73e3ad2ff05cf9ef6227ace
Mushfiq <mumahmud@achieve.com>
2023-10-02 16:38:25 -0400

    chore(neovim): remove view.mappings for nvim-tree new update

--- neovim/.config/nvim/lua/config/plugin-settings/nvim-tree.lua
+++ neovim/.config/nvim/lua/config/plugin-settings/nvim-tree.lua
@@ -62,13 +62,6 @@ nvimtree.setup {
 		width = 30,
 		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
 		side = 'left',
-		mappings = {
-			-- custom only false will merge the list with the default mappings
-			-- if true, it will only use your list to set the mappings
-			custom_only = false,
-			-- list of mappings to set on the tree manually
-			list = {}
-		}
 	},
 
   actions = {
