a4c43f12c6b38ea7e163247c0d4a757afd86cdd4
Mushfiq <mumahmud@achieve.com>
2023-10-03 15:10:00 -0400

    chore(neovim): update indent-blankline config for new version

--- neovim/.config/nvim/lua/config/plugin-settings/indent-blankline.lua
+++ neovim/.config/nvim/lua/config/plugin-settings/indent-blankline.lua
@@ -4,15 +4,9 @@ if not loaded then return end
 vim.opt.list = true
 vim.opt.termguicolors = true
 
-indent_blankline.setup {
+indent_blankline.setup({
+  debounce = 100,
   -- Special Characters: '│' '█' '▏'
-  show_current_context = true,
-  show_current_context_start = true,
-  use_treesitter = true, -- Enable tree sitter to detect current indent level. Needs Treesitter installed
-  disable_with_nolist = true, -- When true, automatically turns this plugin off when |nolist| is set.
-  char = "▏",
-  context_char = "▏",
-  context_char_blankline = "▏",
-  space_char = " ",
-  space_char_blankline = " ",
-}
+  indent = { char = "▏" },
+  whitespace = { highlight = { "Whitespace", "NonText" } },
+})
