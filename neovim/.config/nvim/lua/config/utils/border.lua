e16a5b54d10f95e948b41d1c1550170b6b6262f0
Mushfiq <mumahmud@achieve.com>
2023-10-10 18:12:05 -0400

    chore(neovim): add ability to see what term emulator is being used

--- neovim/.config/nvim/lua/config/utils/border.lua
+++ neovim/.config/nvim/lua/config/utils/border.lua
@@ -1,7 +1,17 @@
+local success, files = pcall(require, 'config/utils/file')
+if not success then return end
+
 local borders = {}
+local term_emulator = nil
+
+local lines = files.lines_from("/Users/mumahmud/dotfiles/.env")
+for _, v in ipairs(lines) do
+  term_emulator = v:match("^TERM_EMULATOR=\"(.+)\"")
+  if term_emulator then break end
+end
 
 -- the characters only work in the kitty terminal
-if vim.loop.os_uname().sysname == "Linux" then
+if term_emulator ~= nil and term_emulator == "kitty" then
   borders = {
     '🭽', '▔', '🭾',
     '▏', ' ', '▕',
