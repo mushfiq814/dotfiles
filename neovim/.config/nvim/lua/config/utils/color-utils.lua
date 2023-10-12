db5c9dd056342afd59ae7497fff07da7e0fdde7f
Mushfiq Mahmud <mumahmud@achieve.com>
2023-10-12 17:59:58 -0400

    chore(neovim): add color utils function to generate colors

--- neovim/.config/nvim/lua/config/utils/color-utils.lua
+++ neovim/.config/nvim/lua/config/utils/color-utils.lua
@@ -57,6 +57,48 @@ function M.darken(hex, pct)
   return string.format("#%s", rgb_to_hex(r, g, b))
 end
 
+--- get the midpoint color between 2 colors
+--- use as colorMidpoint("#ff0000", "#0000ff")
+--- should return "#800080"
+--- @param c1 string should be a valid hex color value starting with #
+--- @param c2 string should be a valid hex color value starting with #
+--- @return string
+function M.colorMidpoint(c1, c2)
+  local c = "#"
+  for i = 2, 7, 2 do
+    local avg = (tonumber(c1:sub(i, i + 1), 16) + tonumber(c2:sub(i, i + 1), 16)) / 2
+    c = c .. string.format("%02x", math.ceil(avg))
+  end
+  return c
+end
+
+--- get in-between colors between 2 colors
+--- use as colorInterpolate("#ff0000", "#0000ff", 6)
+--- should return { "#db0025", "#b70049", "#92006e", "#6e0092", "#4900b7", "#2500db" }
+--- @param c1 string should be a valid hex color value starting with #
+--- @param c2 string should be a valid hex color value starting with #
+--- @param n integer the number of interpolations in the middle
+--- @return table<string>
+function M.colorInterpolate(c1, c2, n)
+  local midColors = {}
+  for i = 1, n, 1 do
+    -- 't' should be a value between 0 and 1 that defines the current
+    -- distance from the first color value
+    local t = 1 / (n + 1) * i
+    local c = "#"
+
+    -- do for each color channel "R", "G", "B"
+    for j = 2, 7, 2 do
+      local x1 = tonumber(c1:sub(j, j + 1), 16)
+      local x2 = tonumber(c2:sub(j, j + 1), 16)
+      local val = x1 + (x2 - x1) * t
+      c = c .. string.format("%02x", math.ceil(val))
+    end
+    table.insert(midColors, c)
+  end
+  return midColors
+end
+
 M.highlight = setmetatable({}, {
   __newindex = function(_, hlgroup, args)
     if ('string' == type(args)) then
