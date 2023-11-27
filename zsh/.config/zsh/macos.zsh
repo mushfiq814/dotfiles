0e8d94ebae17d311a52cf28e05d1c15007af4b85
Mushfiq Mahmud <mumahmud@achieve.com>
2023-11-27 15:19:06 -0500

    chore(zsh): add gcloud and outlook-cli creds/bins

--- zsh/.config/zsh/macos.zsh
+++ zsh/.config/zsh/macos.zsh
@@ -9,6 +9,11 @@ alias BE='cd ~/projects/fplus-loan-app'
 # kubectl/gcp
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 
+# gcloud
+if [ -d "$HOME/google-cloud-sdk/bin" ]; then
+  export PATH="$PATH:$HOME/google-cloud-sdk/bin";
+fi
+
 # fnm (node version manager - alternative to nvm)
 if [ -d "$HOME/Library/Application Support/fnm" ]; then
   export PATH="$HOME/Library/Application Support/fnm:$PATH";
@@ -41,3 +46,7 @@ fi
 if [ -d "$HOME/.spicetify" ]; then
   export PATH="$PATH:$HOME/.spicetify"
 fi
+
+# outlook-cli
+alias outlook="yarn --cwd $HOME/projects/outlook-cli/ --silent run run"
+
