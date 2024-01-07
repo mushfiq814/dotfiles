7358dcd67292e68adb32575dcbdda340f34ef013
Mushfiq Mahmud <mushfiq8194@gmail.com>
2024-01-07 18:30:10 -0500

    chore(zsh): add texlive to PATH

--- zsh/.config/zsh/linux.zsh
+++ zsh/.config/zsh/linux.zsh
@@ -1,3 +1,12 @@
 # aliases
 alias open='xdg-open'
 
+# texlive
+if [ -d "$HOME/.texlive/bin/x86_64-linux" ]; then
+  PATH="$PATH:$HOME/.texlive/bin/x86_64-linux";
+fi
+
+if [ -d "$HOME/.texlive/texmf-dist/doc" ]; then
+  MANPATH="$MANPATH:$HOME/.texlive/texmf-dist/doc/man"
+  INFOPATH="$INFOPATH:$HOME/.texlive/texmf-dist/doc/info"
+fi
