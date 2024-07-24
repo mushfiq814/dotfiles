a438a37374f48d3c401cc0dcfd2a16a61c2fb6dc
Mushfiq Mahmud <mumahmud@achieve.com>
2024-07-24 19:42:30 -0400

    feat(neovim): integrate GitHub Copilot and Copilot Chat
    This commit integrates GitHub Copilot and Copilot Chat into the Neovim
    configuration. It sets up key mappings for Copilot suggestions and
    configures the Copilot Chat plugin with custom prompts for code
    explanation, review, fix, optimization, documentation, and testing. It
    also overrides the git prompts for commit messages to follow the
    commitizen convention. This change enhances the coding experience by
    providing AI-powered assistance directly in the editor.

--- neovim/.config/nvim/lua/config/plugins/dev.lua
+++ neovim/.config/nvim/lua/config/plugins/dev.lua
@@ -125,4 +125,104 @@ return {
       }
     end,
   },
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set('i', '<C-]>', '<Plug>(copilot-dismiss)')         -- Dismiss the current suggestion.
+      vim.keymap.set('i', '<C-i>', '<Plug>(copilot-next)')            -- Cycle to the next suggestion, if one is available.
+      vim.keymap.set('i', '<C-u>', '<Plug>(copilot-previous)')        -- Cycle to the previous suggestion.
+      vim.keymap.set('i', '<C-h>', '<Plug>(copilot-suggest)')         -- Explicitly request a suggestion, even if Copilot is disabled.
+      vim.keymap.set('i', '<C-Right>', '<Plug>(copilot-accept-word)') -- Accept the next word of the current suggestion.
+      vim.keymap.set('i', '<C-Left>', '<Plug>(copilot-accept-line)')  -- Accept the next line of the current suggestion.
+
+      -- copilot_workspace_folders
+      --   A list of "workspace folders" or project roots that
+      --   Copilot may use to improve to improve the quality of
+      --   suggestions.
+      --
+      --   let g:copilot_workspace_folders =
+      --          \ ["~/Projects/myproject"]
+      --
+      --   You can also set b:workspace_folder for an individual
+      --   buffer and newly seen values will be added
+      --   automatically.
+
+      -- vim.api.nvim_set_hl(0, 'CopilotSuggestion', {
+      --   fg = '#555555',
+      --   ctermfg = 8,
+      --   force = true
+      -- })
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    event = "VeryLazy",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    opts = {
+      debug = true,
+      show_help = "yes",
+      model = 'gpt-4', -- GPT model to use, 'gpt-3.5-turbo' or 'gpt-4'
+      temperature = 0.1, -- GPT temperature
+      question_header = '## Mushfiq ', -- Header to use for user questions
+      answer_header = '## Copilot Chat ', -- Header to use for AI answers
+      error_header = '## Error ', -- Header to use for errors
+      separator = '───', -- Separator to use in chat
+      prompts = {
+        Explain = {
+          prompt = '/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text.',
+        },
+        Review = {
+          prompt = '/COPILOT_REVIEW Review the selected code.',
+        },
+        Fix = {
+          prompt = '/COPILOT_GENERATE There is a problem in this code. Rewrite the code to show it with the bug fixed.',
+        },
+        Optimize = {
+          prompt = '/COPILOT_GENERATE Optimize the selected code to improve performance and readablilty.',
+        },
+        Docs = {
+          prompt = '/COPILOT_GENERATE Please add documentation comment for the selection.',
+        },
+        Tests = {
+          prompt = '/COPILOT_GENERATE Please generate tests for my code.',
+        },
+        FixDiagnostic = {
+          prompt = 'Please assist with the following diagnostic issue in file:',
+        },
+      },
+    },
+    config = function(_, opts)
+      local chat = require("CopilotChat")
+      local select = require("CopilotChat.select")
+
+      -- Override the git prompts message
+      local prompt =
+      'Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.'
+      opts.prompts.CommitVisual = {
+        prompt = prompt,
+        selection = select.visual,
+      }
+      opts.prompts.Commit = {
+        prompt = prompt,
+        selection = select.gitdiff,
+      }
+      opts.prompts.CommitStaged = {
+        prompt = prompt,
+        selection = function(source)
+          return select.gitdiff(source, true)
+        end,
+      }
+
+      chat.setup(opts)
+    end,
+  },
 }
