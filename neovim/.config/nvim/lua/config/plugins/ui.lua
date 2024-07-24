0effe390260b19dfce88dc5ff7492a0d31605973
Mushfiq Mahmud <mumahmud@achieve.com>
2024-07-24 19:35:45 -0400

    fix(neovim): adjust twilight plugin settings
    This commit adjusts the settings for the twilight plugin in the Neovim
    configuration. It disables the plugin by default and changes the
    'term_bg' color from '#000000' to '#dbdbdb'. This change improves the
    visibility of text when the twilight mode is active.

--- neovim/.config/nvim/lua/config/plugins/ui.lua
+++ neovim/.config/nvim/lua/config/plugins/ui.lua
@@ -161,13 +161,14 @@ return {
   },
   {
     'folke/twilight.nvim',
+    enabled = false,
     opts = {
       {
         dimming = {
           alpha = 0.50, -- amount of dimming
           -- we try to get the foreground from the highlight groups or fallback color
           color = { "Normal", "#ffffff" },
-          term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
+          term_bg = "#dbdbdb", -- if guibg=NONE, this will be used to calculate text color
           inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
         },
         context = 10,    -- amount of lines we will try to show around the current line
