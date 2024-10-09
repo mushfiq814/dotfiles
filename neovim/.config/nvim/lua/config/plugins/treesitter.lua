2e4b00f81fec78dcae9d3d0268e974099b9d6c6b
Mushfiq Mahmud <mushfiq8194@gmail.com>
2024-10-09 17:57:23 -0400

    remove treesitter directive

--- neovim/.config/nvim/lua/config/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/config/plugins/treesitter.lua
@@ -50,20 +50,3 @@ return {
     },
   },
 }
-
--- WIP
--- add directive for conceal char for code block language defined in highlights file
--- local function conceal_as_devicon(match, _, bufnr, pred, metadata)
---   if #pred == 2 then
---     -- (#as_devicon! @capture)
---     local capture_id = pred[2]
---     local lang = vim.treesitter.get_node_text(match[capture_id], bufnr)
---
---     local icon, color = require("nvim-web-devicons").get_icon_color_by_filetype(lang, { default = true })
---     metadata["conceal"] = icon
---     metadata["color"] = color
---     print(color)
---   end
--- end
---
--- vim.treesitter.query.add_directive("as_devicon!", conceal_as_devicon, true)
